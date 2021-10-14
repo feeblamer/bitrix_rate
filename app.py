79ee764e97e2c3ec8d41cea078d25ef754c05be0
Aleksandr Maslyakov <adeacea@gmail.com>
2021-10-14 18:57:22 +0500

    Change template
    Add headers for tabel of currencies rates

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
-from flask import request
+from flask import render_template
+
+from parser import get_currencies
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def hello_world():
-    return '<p style="color:red;">Hello, World!</p>'
+    data = get_currencies()
+    return render_template(
+            'index.html',
+            date=data['date'],
+            currencies=data['currencies'])
 
-@app.route("/app", methods=['POST'])
-def bitrix_app():
-    return '<h1>opa</h1>'
 
 if __name__=="__main__":
     app.run("0.0.0.0")
